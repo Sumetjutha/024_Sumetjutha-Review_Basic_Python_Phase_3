a9485bf7bba2077aa51644db235ff4a153750743
Sumet Jutajan <sumet.jutha@gmail.com>
2023-06-09 23:13:32 +0700

    update

---
+++ ep13_Package/MainProgram.py
@@ -0,0 +1,5 @@
+from Package.CalculateService import addition
+from Package.Weather import city
+
+addition(10,20,30,40)
+print(city)
---
+++ ep13_Package/Package/CalculateService.py
@@ -0,0 +1,15 @@
+# Service ค่าคงที่ 
+PI = 3.14
+ROOT = 1.414
+
+def addition(*args):
+    total=0
+    for i in range(len(args)):
+        total+=args[i]
+    print("ผลบวกมีค่าเท่ากับ = ",total)
+    
+def subtraction(num1,num2):
+    print("ผลลบมีค่าเท่ากับ = ", (num1 - num2))
+    
+def power(num1,num2):
+    print(num1**num2)
