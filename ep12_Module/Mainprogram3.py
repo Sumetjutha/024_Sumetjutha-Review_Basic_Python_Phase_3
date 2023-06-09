18e6b82facb35a96b99a1e09fece6525a99b0938
Sumet Jutajan <sumet.jutha@gmail.com>
2023-06-09 23:02:53 +0700

    update

--- ep12_Module/Mainprogram3.py
+++ ep12_Module/Mainprogram3.py
@@ -1,6 +1,14 @@
 # เรียก function เฉพาะที่เราต้องการ
 from CalculateService import addition
 from CalculateService import PI
+from Weather import city
+
+# ดึงมาทั้งหมด
+from Weather import *
+from CalculateService import *
 
 addition(50,150,200)
 print(PI)
+print(city)
+
+power(5,6)
